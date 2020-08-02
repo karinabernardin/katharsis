ce4df6f4c40c24e128a4a42c67b17fcfaf2a74ef
karinabernardin <karinabernardin@gmail.com>
2020-08-02 12:32:30 -0300

    Add details to the category registration page.

--- src/pages/Registration/Category/index.js
+++ src/pages/Registration/Category/index.js
@@ -6,9 +6,22 @@ import { Link } from 'react-router-dom'
 function CategoryRegistration() {
     return(
       <PageDefault>
-        <h1>
-        Category Registration
-        </h1>
+        <h1>Category Registration</h1>
+
+        <form>
+
+          <label>
+            Name of the Category:
+            <input
+              type="text"
+            />
+          </label>
+
+          <button>
+            Register
+          </button>
+
+        </form>
 
         {/* link to another component without reloading the page (Single Page Applcation) */}
         <Link to="/">
