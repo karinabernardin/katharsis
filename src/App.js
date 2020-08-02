6459bcbd1c1cd50d160b19601323fdfb608e165a
karinabernardin <karinabernardin@gmail.com>
2020-08-01 22:45:20 -0300

    Include the use of React Router in the project. Start the routing of the pages.

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={"Lala"}
+        // videoDescription={"Lala"}
       />
 
       <Carousel
--- src/index.js
+++ src/index.js
@@ -2,10 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
+
+// every component is a function!
+function Register() {
+  return(
+    <div>
+      Video Registering Page
+    </div>
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/register/video" component={Register} exact />
+      <Route path="/" component={App} exact />
+      {/* if there is no route match */}
+      <Route component={() => (<div> Error 404</div>)} /> 
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
